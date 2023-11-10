42e2329ee6f87575c9236202443b4a91e2371c17
shaulov <shineginn@gmail.com>
2023-11-10 21:53:00 +0300

    Change layout

--- app/(marketing)/_components/footer.tsx
+++ app/(marketing)/_components/footer.tsx
@@ -1,13 +1,18 @@
 import Link from "next/link";
 import { Logo } from "./logo";
+import { Button } from "@/components/ui/button";
 
 export function Footer() {
   return (
     <footer className="z-10 flex itemx-center w-full p-6 bg-background dark:bg-[#1F1F1F]">
       <Logo />
       <div className="flex items-center justify-between md:justify-end gap-x-2 w-full md:ml-auto text-muted-foreground">
-        <Link href="">Privacy Policy</Link>
-        <Link href="">Terms & Conditions</Link>
+        <Button variant="ghost" asChild>
+          <Link href="">Privacy Policy</Link>
+        </Button>
+        <Button variant="ghost" asChild>
+          <Link href="">Terms & Conditions</Link>
+        </Button>
       </div>
     </footer>
   );
