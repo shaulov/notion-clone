2bc3630db582c62379a0529f5869cdfee9ba3b3d
shaulov <shineginn@gmail.com>
2023-11-10 21:50:48 +0300

    Add authentication and change layout

--- app/(marketing)/_components/navbar.tsx
+++ app/(marketing)/_components/navbar.tsx
@@ -1,14 +1,19 @@
 "use client";
 
+import Link from "next/link";
+import { useConvexAuth } from "convex/react";
+import { SignInButton, UserButton } from "@clerk/clerk-react";
 import { cn } from "@/lib/utils";
-import { SunIcon } from "lucide-react";
 import { useScrollTop } from "@/hooks/use-scroll-top";
 import { Button } from "@/components/ui/button";
 import { Logo } from "./logo";
 import { ModeToggle } from "@/components/mode-toggle";
+import { Spinner } from "@/components/spinner";
 
 export function Navbar() {
+  const { isAuthenticated, isLoading } = useConvexAuth();
   const scrolled = useScrollTop();
+
   return (
     <header className={cn(
       "fixed top-0 z-50 flex items-center w-full p-6 bg-background dark:bg-[#1F1F1F]",
@@ -17,12 +22,41 @@ export function Navbar() {
       <nav className="flex justify-between w-full">
         <Logo />
         <ul className="flex items-center justify-between gap-x-2 w-full md:w-auto">
-          <li>
-            <Button variant="ghost">Log in</Button>
-          </li>
-          <li>
-            <Button variant="default">Get Jotion free</Button>
-          </li>
+          {isLoading && (
+            <li>
+              <Spinner />
+            </li>
+          )}
+          {!isAuthenticated && !isLoading && (
+            <>
+              <li>
+                <SignInButton mode="modal">
+                  <Button variant="ghost">Log in</Button>
+                </SignInButton>
+              </li>
+              <li>
+                <SignInButton mode="modal">
+                  <Button size="sm">Get Jotion free</Button>
+                </SignInButton>
+              </li>
+            </>
+          )}
+          {isAuthenticated && !isLoading && (
+            <>
+              <li>
+                <Button variant="ghost" size="sm" asChild>
+                  <Link href="/documents">
+                    Enter Jotion
+                  </Link>
+                </Button>
+              </li>
+              <li>
+                <UserButton
+                  afterSignOutUrl="/"
+                />
+              </li>
+            </>
+          )}
           <li>
             <ModeToggle />
           </li>
--- app/(marketing)/_components/promo.tsx
+++ app/(marketing)/_components/promo.tsx
@@ -1,9 +1,15 @@
 "use client";
 
+import Link from "next/link";
+import { useConvexAuth } from "convex/react";
+import { SignInButton } from "@clerk/clerk-react";
 import { ArrowRightIcon } from "lucide-react";
 import { Button } from "@/components/ui/button";
+import { Spinner } from "@/components/spinner";
 
 export function Promo() {
+  const { isAuthenticated, isLoading } = useConvexAuth();
+
   return (
     <section className="max-w-3xl space-y-4">
       <h1 className="text-3xl sm:text-5xl md:text-6xl font-bold">
@@ -14,10 +20,27 @@ export function Promo() {
         Jotion is the connected workspace where <br />
         better, faster work happens
       </p>
-      <Button>
-        Enter Jotion
-        <ArrowRightIcon className="h-4 w-4 ml-2" />
-      </Button>
+      {isLoading && (
+        <div className="flex items-center justify-center w-full">
+          <Spinner size="lg" />
+        </div>
+      )}
+      {isAuthenticated && !isLoading && (
+        <Button asChild>
+          <Link href="/documents">
+            Enter Jotion
+            <ArrowRightIcon className="h-4 w-4 ml-2" />
+          </Link>
+        </Button>
+      )}
+      {!isAuthenticated && !isLoading && (
+        <SignInButton mode="modal">
+          <Button>
+            Geet Jotion free
+            <ArrowRightIcon className="h-4 w-4 ml-2" />
+          </Button>
+        </SignInButton>
+      )}
     </section>
   );
 }
