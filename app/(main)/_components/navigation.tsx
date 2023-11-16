6b7badba465de5bf6cd4ab7dc5e07e1ffef0043d
shaulov <shineginn@gmail.com>
2023-11-16 13:05:31 +0300

    Add collapse and resetWidth functionality

--- app/(main)/_components/navigation.tsx
+++ app/(main)/_components/navigation.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { ElementRef, useRef, useState } from "react";
+import { ElementRef, useEffect, useRef, useState } from "react";
 import { usePathname } from "next/navigation";
 import { ChevronsLeft, MenuIcon } from "lucide-react";
 import { useMediaQuery } from "usehooks-ts";
@@ -12,9 +12,17 @@ export function Navigation() {
   const isMobile = useMediaQuery("(max-width: 768px)");
   const isResizingRef = useRef(false);
   const sidebarRef = useRef<ElementRef<"nav">>(null);
-  const navigationRef = useRef<ElementRef<"div">>(null);
+  const headerRef = useRef<ElementRef<"header">>(null);
   const [isResetting, setIsResetting] = useState(false);
   const [isCollapsed, setIsCollapsed] = useState(isMobile);
+
+  useEffect(() => {
+    isMobile ? collapse() : resetWidth();
+  }, [isMobile]);
+
+  useEffect(() => {
+    if (isMobile) collapse();
+  }, [isMobile, pathname]);
 
   const handleMouseDown = (evt: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
     evt.preventDefault();
@@ -32,10 +40,10 @@ export function Navigation() {
     if (newWidth < 240) newWidth = 240;
     if (newWidth > 480) newWidth = 480;
 
-    if (sidebarRef.current && navigationRef.current) {
+    if (sidebarRef.current && headerRef.current) {
       sidebarRef.current.style.width = `${newWidth}px`;
-      navigationRef.current.style.setProperty("left", `$${newWidth}px`);
-      navigationRef.current.style.setProperty("width", `calc(100% - ${newWidth}px)`);
+      headerRef.current.style.setProperty("left", `$${newWidth}px`);
+      headerRef.current.style.setProperty("width", `calc(100% - ${newWidth}px)`);
     } 
   };
 
@@ -43,6 +51,36 @@ export function Navigation() {
     isResizingRef.current = false;
     document.removeEventListener("mousemove", handleMouseMove);
     document.removeEventListener("mouseup", handleMouseUp);
+  }
+
+  const resetWidth = () => {
+    if (sidebarRef.current && headerRef.current) {
+      setIsCollapsed(false);
+      setIsResetting(true);
+
+      sidebarRef.current.style.width = isMobile ? "100%" : "240px";
+      headerRef.current.style.setProperty(
+        "width",
+        isMobile ? "0" : "calc(100% - 240px)"
+      );
+      headerRef.current.style.setProperty(
+        "left",
+        isMobile ? "100%" : "240px"
+      );
+      setTimeout(() => setIsResetting(false), 300);
+    }
+  };
+
+  const collapse = () => {
+    if (sidebarRef.current && headerRef.current) {
+      setIsCollapsed(true);
+      setIsResetting(true);
+
+      sidebarRef.current.style.width = "0";
+      headerRef.current.style.setProperty("width", "100%");
+      headerRef.current.style.setProperty("left", "0");
+      setTimeout(() => setIsResetting(false), 300);
+    }
   }
 
   return (
@@ -60,6 +98,7 @@ export function Navigation() {
             "absolute top-3 right-2 w-6 h-6 text-muted-foreground rounded-sm opacity-0 group-hover/sidebar:opacity-100 hover:bg-neutral-300 dark:hover:bg-neutral-600",
             isMobile && "opacity-100"
           )}
+          onClick={collapse}
         >
           <ChevronsLeft className="w-6 h-6" />
         </button>
@@ -69,26 +108,26 @@ export function Navigation() {
         </ul>
         <button 
           className="absolute top-0 right-0 w-1 h-full bg-primary/10 cursor-ew-resize transition opacity-0 group-hover/sidebar:opacity-100"
-          onClick={() => {}}
+          onClick={resetWidth}
           onMouseDown={handleMouseDown}
         />
       </nav>
-      <div
+      <header
         className={cn(
           "absolute top-0 left-60 z-10 w-[calc(100%-240px)]",
           isResetting && "transition-all ease-in-out duration-300",
           isMobile && "left-0 w-full"
         )}
-        ref={navigationRef}
+        ref={headerRef}
       >
-        <nav className="w-full px-3 py-2 bg-transparent">
+        <div className="w-full px-3 py-2 bg-transparent">
         {isCollapsed && (
-            <Button variant="ghost">
+            <Button variant="ghost" onClick={resetWidth}>
               <MenuIcon className="w-6 h-6 text-muted-foreground" />
             </Button>
           )}
-        </nav>
-      </div>
+        </div>
+      </header>
     </>
   );
 }
