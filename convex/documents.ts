959bd0623c0df5b9a3ad0f3345329b054d9fcaa6
shaulov <shineginn@gmail.com>
2023-12-06 13:47:25 +0300

    Add API for updating title of document

--- convex/documents.ts
+++ convex/documents.ts
@@ -197,4 +197,34 @@ export const getById = query({
 
     return document;
   },
+});
+
+export const update = mutation({
+  args: {
+    id: v.id("documents"),
+    title: v.optional(v.string()),
+    content: v.optional(v.string()),
+    coverImage: v.optional(v.string()),
+    icon: v.optional(v.string()),
+    isPublished: v.optional(v.boolean()),
+  },
+  handler: async (ctx, args) => {
+    const identity = await ctx.auth.getUserIdentity();
+
+    if (!identity) throw new Error("Not authenticated");
+
+    const userId = identity.subject;
+    const { id, ...rest } = args;
+
+    const existingDocument = await ctx.db.get(id);
+
+    if (!existingDocument) throw new Error("Not found");
+    if (existingDocument.userId !== userId) throw new Error("Unauthorized");
+
+    const document = await ctx.db.patch(id, {
+      ...rest,
+    });
+
+    return document;
+  },
 })
